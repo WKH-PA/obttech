94b7c8417e504fb7e9f625040aff08cd6a49637c
Minh bÉO đây =)) <minhgk2002@gmail.com>
2024-08-06 17:12:30 +0700

    1400812

--- sources/sitemap.php
+++ sources/sitemap.php
@@ -1,29 +1,33 @@
 <?php
-	// header('Content-Type: text/plain');
-	header("Content-type: text/xml");
-	echo '<?xml version="1.0" encoding="UTF-8"?>
+//	 header('Content-Type: text/plain');
+header("Content-type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>
+
 	<urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
-	$sitemap = '<url>
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+$sitemap = '<url>
 	        <loc>'.$fullpath.'</loc>
+	        <lastmod>2021-01-27</lastmod>
+	        <changefreq>weekly</changefreq>
 	        <priority>1</priority>
 	      </url>';
 
-	    $arrsitemap = DB_que("SELECT * FROM `#_slug` ORDER BY `id` DESC");
-	    while($row  = mysqli_fetch_array($arrsitemap))
-	    {
-	      	$sitemap .= '<url>
+$arrsitemap = DB_que("SELECT * FROM `#_slug` ORDER BY `id` DESC");
+while($row  = mysqli_fetch_array($arrsitemap))
+{
+    $sitemap .= '<url>
 	             <loc>'.$fullpath."/".$row['slug'].'/</loc>
+	             <lastmod>2021-01-27</lastmod>
+	             <changefreq>weekly</changefreq>
 	             <priority>'.(RAND(111,999)/1000).'</priority>
 	            </url>';
-	        // $sitemap .= '<url>
-	        //      <loc>'.$fullpath."/en/".$row['slug'].'/</loc>
-	        //      <priority>'.(RAND(111,999)/1000).'</priority>
-	        //     </url>';
-	    }
-	    $sitemap .= '</urlset>';
-	echo $sitemap;
+    // $sitemap .= '<url>
+    //      <loc>'.$fullpath."/en/".$row['slug'].'/</loc>
+    //      <priority>'.(RAND(111,999)/1000).'</priority>
+    //     </url>';
+}
+$sitemap .= '</urlset>';
+echo $sitemap;
 ?>
