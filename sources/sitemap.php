f89bd7e1e21c8967589e5fa111eb2f9dddc13e71
Minh bÉO đây =)) <minhgk2002@gmail.com>
2024-08-08 13:12:54 +0700

    1400812 - Kế hoạch

--- sources/sitemap.php
+++ sources/sitemap.php
@@ -1,29 +1,28 @@
 <?php
-	// header('Content-Type: text/plain');
-	header("Content-type: text/xml");
-	echo '<?xml version="1.0" encoding="UTF-8"?>
+header("Content-type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>
 	<urlset
+	  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
-	$sitemap = '<url>
+$sitemap = '<url>
 	        <loc>'.$fullpath.'</loc>
-	        <priority>1</priority>
+	        <priority>1.00</priority>
 	      </url>';
 
-	    $arrsitemap = DB_que("SELECT * FROM `#_slug` ORDER BY `id` DESC");
-	    while($row  = mysqli_fetch_array($arrsitemap))
-	    {
-	      	$sitemap .= '<url>
+$arrsitemap = DB_que("SELECT * FROM `#_slug` ORDER BY `id` DESC");
+while($row  = mysqli_fetch_array($arrsitemap))
+{
+    $sitemap .= '<url>
 	             <loc>'.$fullpath."/".$row['slug'].'/</loc>
-	             <priority>'.(RAND(111,999)/1000).'</priority>
+	             <priority>'.sprintf("%.2f",RAND(111,999)/1000).'</priority>
 	            </url>';
-	        // $sitemap .= '<url>
-	        //      <loc>'.$fullpath."/en/".$row['slug'].'/</loc>
-	        //      <priority>'.(RAND(111,999)/1000).'</priority>
-	        //     </url>';
-	    }
-	    $sitemap .= '</urlset>';
-	echo $sitemap;
+    // $sitemap .= '<url>
+    //      <loc>'.$fullpath."/en/".$row['slug'].'/</loc>
+    //      <priority>'.(RAND(111,999)/1000).'</priority>
+    //     </url>';
+}
+$sitemap .= '</urlset>';
+echo $sitemap;
 ?>
